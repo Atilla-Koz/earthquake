2706aa0344f28391638ca89bcead5cae093921b2
Atilla Köz <115349531+Atilla-Koz@users.noreply.github.com>
2025-04-24 20:19:13 +0300

    Navbar bileşenine mobil uyumlu menü açma/kapama işlevi eklendi. CSS stilleri güncellenerek menü simgesi ve görünüm iyileştirildi. Navbar'ın yapısı ve stilleri, daha iyi bir kullanıcı deneyimi sağlamak için düzenlendi.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,45 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Navbar.css'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           Deprem Takip
         </Link>
-        <ul className="nav-menu">
+        
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+        </div>
+
+        <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
           <li className="nav-item">
-            <Link to="/" className="nav-link">
+            <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               Ana Sayfa
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/haritalar" className="nav-link">
+            <Link to="/haritalar" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               Haritalar
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/istatistikler" className="nav-link">
+            <Link to="/istatistikler" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               İstatistikler
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/hakkimizda" className="nav-link">
+            <Link to="/hakkimizda" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               Hakkımızda
             </Link>
           </li>
